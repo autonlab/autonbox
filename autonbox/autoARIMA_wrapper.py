6605a2b2eef7bd0e6a4fa4653163336296716a6a
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2022-07-14 12:51:58 -0400

    Update metadata

--- autonbox/autoARIMA_wrapper.py
+++ autonbox/autoARIMA_wrapper.py
@@ -1,14 +1,17 @@
+import os
+
 from d3m import container
-from d3m.primitive_interfaces import base, transformer
+from d3m import utils as d3m_utils
+from d3m.primitive_interfaces import base
+from d3m.primitive_interfaces.supervised_learning import SupervisedLearnerPrimitiveBase
 from d3m.metadata import base as metadata_base, hyperparams
 
-from quickstart_primitives.config_files import config
+import autonbox
 
-__all__ = ('InputToOutputPrimitive',)
+__all__ = ('AutoARIMAWrapperPrimitive',)
 
 Inputs = container.DataFrame
 Outputs = container.DataFrame
-
 
 class Hyperparams(hyperparams.Hyperparams):
     """
@@ -17,26 +20,32 @@ class Hyperparams(hyperparams.Hyperparams):
     pass
 
 
-class InputToOutputPrimitive(transformer.TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
+class AutoARIMAWrapperPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outputs, Hyperparams]):
     """
     A sample primitive. The output dataframe is the input dataframe.
     """
 
     metadata = hyperparams.base.PrimitiveMetadata({
-        "id": "30d5f2fa-4394-4e46-9857-2029ec9ed0e0",
-        "version": config.VERSION,
-        "name": "Passthrough primitive",
-        "description": "A primitive which directly outputs the input.",
-        "python_path": "d3m.primitives.operator.input_to_output.Quickstart",
-        "primitive_family": metadata_base.PrimitiveFamily.OPERATOR,
-        "algorithm_types": [metadata_base.PrimitiveAlgorithmType.IDENTITY_FUNCTION],
-        "source": {
-            "name": config.AUTHOR_NAME,
-            "contact": config.AUTHOR_CONTACT,
-            "uris": [config.REPOSITORY],
+        "id": "434d4d25-dd61-4a32-a624-0f983995e189",
+        "version": "0.1.0",
+        "name": "AutoARIMA Wrapper",
+        "description": "Wrapper of the AutoARIMA class from statsforecast package",
+        "python_path": "d3m.primitives.time_series_forecasting.arima.AutonBox",
+        "primitive_family": metadata_base.PrimitiveFamily.TIME_SERIES_FORECASTING,
+        "algorithm_types": [], #TODO add these
+        'source': {
+            'name': autonbox.__author__,
+            'uris': ['https://github.com/autonlab/autonbox'],
+            'contact': 'mailto:mkowales@andrew.cmu.edu'
         },
-        "keywords": ["passthrough"],
-        "installation": [config.INSTALLATION],
+        "keywords": ["ARIMA", "time series", "forecasting"],
+        "installation": [{
+            'type': metadata_base.PrimitiveInstallationType.PIP,
+
+            'package_uri': 'git+https://github.com/autonlab/autonbox.git@{git_commit}#egg=autonbox'.format(
+                git_commit=d3m_utils.current_git_commit(os.path.dirname(__file__)),
+            ),
+        }]
     })
 
     def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> base.CallResult[Outputs]:
